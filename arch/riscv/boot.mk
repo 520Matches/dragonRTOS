f98cdd0fa2bcd497d0c8d2716c02176de81639bc
520Matches <HengDi_Sun@163.com>
2024-03-21 16:30:37 +0800

    add bss init

--- arch/riscv/boot.mk
+++ arch/riscv/boot.mk
@@ -1,16 +1,23 @@
 BOOT_DIR := $(ARCH_DIR)/boot
 
-BOOT-O := head.o
-BOOT-O += boot.o
+C-FILES := $(wildcard $(BOOT_DIR)/*.c)
+ASM-FILES := $(wildcard $(BOOT_DIR)/*.S)
 
-dragon.elf: $(BOOT-O)
+# FILES-O := $(subst .c,.o,$(C-FILES))
+# FILES-O += $(subst .S,.o,$(ASM-FILES))
+
+FILES-O := 
+FILES-O += head.o
+FILES-O += boot.o
+
+dragon.elf: $(FILES-O)
 	$(LD) -T $(BOOT_DIR)/boot_linker.ld -o $@ $^
 
 head.o: $(BOOT_DIR)/head.S
 	$(GCC) -c $<
 
 boot.o: $(BOOT_DIR)/boot.c
-	$(GCC) -c $<
+	$(GCC) -c $(CFLAGS) $<
 
 
 bootclean:
--- arch/riscv/boot/boot.c
+++ arch/riscv/boot/boot.c
@@ -1,5 +1,23 @@
+#include "../../../include/types.h"
+
+extern uint32_t _bss_begin;
+extern uint32_t _bss_end;
+
+void bss_clear(void)
+{
+	uint32_t *p;
+	for(p = &_bss_begin; p < &_bss_end;)
+	{
+		*p++ = 0;
+	}
+}
+
 int main(void)
 {
 	int ret = 0;
+
+	// clear bss
+	bss_clear();
+
 	return ret;
 }
